567a8bd5b2afae314438c0234d919ca5262cce2b
shengxc <sheng_xc@126.com>
2014-03-01 13:17:36 +0800

    some small change in cntvcrawl

---
+++ cntvcrawl/README.md
@@ -0,0 +1,3 @@
+a spider to download videos' title,time,image url and video urls on search.cctv.cn
+USAGE:
+  scrapy crawl cntvspider -a keyword=<keyword>
--- cntvcrawl/cntvcrawl/pipelines.py
+++ cntvcrawl/cntvcrawl/pipelines.py
@@ -8,16 +8,19 @@ class CntvcrawlPipeline(object):
   conn = None
   cursor = None
   def open_spider(self,spider):
+    #self.outf = open("sql.log","w")
     self.conn = MySQLdb.Connect(host="10.214.55.244",user="project973",passwd="008059c20c2ec4bca8e18ce5c9e2bea0",db="project863_20140223",charset="utf8")
 
     self.cursor = self.conn.cursor()
 
   def process_item(self, item, spider):
     sql = "INSERT INTO videos (title,video_url,img_url,published_at) VALUES ('%s','%s','%s','%s')" % (item["title"],item["videourl"],item["imgurl"],item["time"])
+    #self.outf.write((sql + "\n").encode("utf-8"))
     self.cursor.execute(sql)
     self.conn.commit()
     return item
 
   def close_spider(self,spider):
+    #self.outf.close()
     self.cursor.close()
     self.conn.close()
