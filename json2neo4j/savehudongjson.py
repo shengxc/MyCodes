0296a01aac8d9847f3a003bb8012ff39088887e9
shengxc <sheng_xc@126.com>
2014-03-14 23:25:24 +0800

    modified json2neo4j

--- json2neo4j/savehudongjson.py
+++ json2neo4j/savehudongjson.py
@@ -11,26 +11,9 @@ import os
 import re
 from neo4jrestclient.client import GraphDatabase
 gdb = GraphDatabase("http://localhost:7474/db/data/")
-fout = open("run.log","a")
-'''
-replacemap = {u'0':u'零',u'1':u'一',u'2':u'二',u'3':u'三',u'4':u'四',u'5':u'五',u'6':u'六',u'7':u'七',u'8':u'八',u'9':u'九',u'"':u'',u'（':u'',u'）':u'',u'“':u'',u'”':u'',u'-':u''}
-def makedictrepl(inputdict):
-  rx = re.compile('|'.join(inputdict))
-  def eachrepl(match):
-    return inputdict[match.group(0)]
-  def repl(text):
-    return rx.sub(eachrepl,text)
-  return repl
-typereplacefunc = makedictrepl(replacemap)
-def dealtype(elemtype):
-  elemtype = elemtype.strip()
-  res = elemtype.replace(u".",u"")    #delete . character in type
-  res = typereplacefunc(elemtype)
-  if elemtype != res:
-    fout.write(("typechange:" + elemtyp + "-->" + res + "\n").encode("utf-8"))
-    fout.flush()
-  return res
-'''
+fout = open("run.log","w")
+dealf = open("deal.log","w")
+
 def dealname(nodename):
   nodename = nodename.strip()
   res = nodename.replace("\\",",").replace('"','\\"')
@@ -47,26 +30,35 @@ def execute(query):
     pass
 
 def addnode(nodetype,nodename):
-  assert(nodetype == 'ENTITY' or nodetype == 'CATEGORY')
-  def isexist(nodetype,nodename):
+  def isexistnode(nodetype,nodename):
     testexistquery = '''match (n:%s {name:"%s"}) return count(n)''' % (nodetype,nodename)
     res = execute(testexistquery)
     return res[0][0] > 0
-  if not isexist(nodetype,nodename):
+  assert(nodetype == 'ENTITY' or nodetype == 'CATEGORY')
+  if not isexistnode(nodetype,nodename):
     addnodequery = '''create (n:%s {name:"%s"})''' % (nodetype,nodename)
     execute(addnodequery)
 
 def addrel(nodename1,nodename2,reltype,relname=None):
+  def isexistrel(nodename1,nodename2,reltype,relname=None):
+    if reltype == 'PROPERTY':
+      testrelquery = '''match (ent:ENTITY {name:"%s"})-[rel:%s {name:"%s"}]->(att:ENTITY {name:"%s"}) return count(rel) ''' % (nodename1,reltype,relname,nodename2)
+    else:
+      testrelquery = '''match (ent:ENTITY {name:"%s"})-[rel:%s]->(att:ENTITY {name:"%s"}) return count(rel) ''' % (nodename1,reltype,nodename2)
+    res = execute(testrelquery)
+    return res[0][0] > 0
   assert(reltype == 'PROPERTY' or reltype == 'CATEGORY')
-  if reltype == 'PROPERTY':
-    addrelquery = '''match (ent:ENTITY {name:"%s"}),(att:ENTITY {name:"%s"}) create ent-[:%s {name:"%s"}]->att''' % (nodename1,nodename2,reltype,relname)
-  else:
-    addrelquery = '''match (ent:ENTITY {name:"%s"}),(cate:CATEGORY {name:"%s"}) create ent-[:%s]->cate''' % (nodename1,nodename2,reltype)
-  execute(addrelquery)
+  if not isexistrel(nodename1,nodename2,reltype,relname):
+    if reltype == 'PROPERTY':
+      addrelquery = '''match (ent:ENTITY {name:"%s"}),(att:ENTITY {name:"%s"}) create ent-[:%s {name:"%s"}]->att''' % (nodename1,nodename2,reltype,relname)
+    else:
+      addrelquery = '''match (ent:ENTITY {name:"%s"}),(cate:CATEGORY {name:"%s"}) create ent-[:%s]->cate''' % (nodename1,nodename2,reltype)
+    execute(addrelquery)
 
 hudongfilename = "/media/sxc/Seagate Backup Plus Drive/EncyclopediaData/hudongbaikeknowledgesmerge.json"
 with open(hudongfilename) as inputfile:
   items = json.load(inputfile)
+index = 0
 for item in items:
   entry = dealname(item["entry"])       #unicode
   categories = item["categories"]     #list
@@ -85,4 +77,8 @@ for item in items:
       continue
     addnode("ENTITY",value)
     addrel(entry,value,"PROPERTY",prop)
+  dealf.write((str(index) + "\n").encode("utf-8"))
+  dealf.flush()
+  index = index + 1
 fout.close()
+dealf.close()
